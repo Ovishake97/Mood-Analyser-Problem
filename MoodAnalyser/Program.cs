96401b4280fece12e795564098d20cbb9c56238a
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-12 14:53:14 +0530

    [Initial commit]

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey! How's your mood today?");
+            string message = Console.ReadLine();
+            MoodAnalyse moodAnalyser = new MoodAnalyse(message);
+            Console.WriteLine($"Looks like you are " +moodAnalyser.analyseMood());
+        }
+    }
+
+    public class MoodAnalyse {
+        public string message;
+        public MoodAnalyse(string message) {
+            this.message = message;
+        }
+        public string analyseMood() {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "sad";
+            }
+            else {
+                return "happy";
+            }
+        }
+    }
+}
